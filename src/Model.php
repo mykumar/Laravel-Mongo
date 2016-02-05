47fac64c3bd60695125cd82442c0f0653f0d76c9
Yuriy G <yuriyg86@gmail.com>
2016-02-05 18:28:16 +0200

    First

---
+++ src/Model.php
@@ -0,0 +1,11 @@
+<?php namespace MasterYuri\LaravelMongo;
+
+// use Jenssegers\Mongodb;
+
+class Model //extends Jenssegers\Mongodb\Model {
+{
+    public function hasCheese($bool = true)
+    {
+        return $bool;
+    }
+}
